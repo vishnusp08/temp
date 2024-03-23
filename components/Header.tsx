ae26dfcebee3750ab169a1ceca9342c0b29b59b8
Prajwalg19 <theimperical8@gmail.com>
2024-03-23 16:47:32 +0530

    context api initialization

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,28 +5,17 @@ import { useState } from "react";
 import Link from "next/link";
 
 export default function Header() {
-  const [focus, setFocus] = useState("#home");
+    const [focus, setFocus] = useState("#home");
 
-  return (
-    <motion.header
-      className="shadow-lg fixed left-1/2 sm:top-[2rem] top-0 sm:w-[40rem] bg-white w-full sm:rounded-full z-[100]"
-      initial={{ y: -110, x: "-50%", opacity: 0 }}
-      animate={{ y: 3, x: "-50%", opacity: 0.937 }}
-    >
-      <ul className="flex flex-wrap items-center justify-around w-full h-auto p-2 font-semibold transition-all sm:p-0 sm:h-14 box-border">
-        {links.map((item) => (
-          <Link key={item.hash} href={item.hash}>
-            <motion.li
-              className={`box-border text-gray-600 px-4 py-2 rounded-full hover:text-gray-900 cursor-ointer ${
-                focus == item.hash && "bg-slate-200"
-              }`}
-              onClick={() => setFocus(item.hash)}
-            >
-              {item.name}
-            </motion.li>
-          </Link>
-        ))}
-      </ul>
-    </motion.header>
-  );
+    return (
+        <motion.header className="shadow-lg fixed left-1/2 sm:top-[2rem] top-0 sm:w-[40rem] bg-white w-full sm:rounded-full z-[100]" initial={{ y: -110, x: "-50%", opacity: 0 }} animate={{ y: 3, x: "-50%", opacity: 0.937 }}>
+            <ul className="flex flex-wrap items-center justify-around w-full h-auto p-2 font-semibold transition-all sm:p-0 sm:h-14 box-border">
+                {links.map((item) => (
+                    <Link key={item.hash} href={item.hash} onClick={() => setFocus(item.hash)}>
+                        <motion.li className={`box-border text-gray-600 px-4 py-2 rounded-full hover:text-gray-900 cursor-ointer ${focus == item.hash && "bg-slate-200"}`}>{item.name}</motion.li>
+                    </Link>
+                ))}
+            </ul>
+        </motion.header>
+    );
 }

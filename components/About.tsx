a828be25f60ca52e21ec6c1cea23e0826567ac57
Prajwalg19 <theimperical8@gmail.com>
2024-03-27 00:01:27 +0530

    changing apostrpohe (apos;)

--- components/About.tsx
+++ components/About.tsx
@@ -1,22 +1,22 @@
 "use client";
 import Section_headers from "@/components/Section_headers";
-import { motion } from "framer-motion";
-import { useSectionInview } from "@/library/customHooks";
+import {motion} from "framer-motion";
+import {useSectionInview} from "@/library/customHooks";
 export default function About() {
-    const { ref } = useSectionInview({ thres: 0.8, sectionTag: "#about" });
+    const {ref} = useSectionInview({thres: 0.8, sectionTag: "#about"});
     return (
         <motion.main
             ref={ref}
             id="about" // when I add  an id equal to the route, the routes points to this section of the page
             className="w-full max-w-5xl mx-auto mb-28 sm:mb-0 scroll-mt-36"
-            initial={{ y: 100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ delay: 0.15 }}
+            initial={{y: 100, opacity: 0}}
+            animate={{y: 0, opacity: 1}}
+            transition={{delay: 0.15}}
         >
             <Section_headers>About Me</Section_headers>
             <section className="text-lg text-center leading-8">
                 <p>
-                    Hey there! I'm <strong>Prajwal</strong>, a passionate CS undergraduate and a <strong>full stack developer</strong>. I have a knack for building robust and scalable web applications using modern technologies. With a keen interest in problem-solving, I thrive on challenges and love
+                    Hey there! I&apos;m <strong>Prajwal</strong>, a passionate CS undergraduate and a <strong>full stack developer</strong>. I have a knack for building robust and scalable web applications using modern technologies. With a keen interest in problem-solving, I thrive on challenges and love
                     to tackle complex problems.
                 </p>
 
@@ -29,7 +29,7 @@ export default function About() {
 
                 <p>
                     <strong>Why I Love What I Do:</strong>
-                    <br />I am deeply passionate about technology and its potential to shape the world. Whether it's creating intuitive user interfaces or designing efficient backend systems, I find joy in every step of the development process.
+                    <br />I am deeply passionate about technology and its potential to shape the world. Whether it&apos;s creating intuitive user interfaces or designing efficient backend systems, I find joy in every step of the development process.
                 </p>
             </section>
         </motion.main>
--- components/btn-comp.tsx
+++ components/btn-comp.tsx
@@ -1,9 +1,9 @@
 // this is a different component because the useFormStatus hook has to be used within the form element but hook cannot be used inside of the jsx hence we export the btn compoenet and also usage of this useFormStatus is possible. useFormStatus porvides the porcessing state of the form with a boolean
 // value
-import { FaRegPaperPlane } from "react-icons/fa";
-import { useFormStatus } from "react-dom";
+import {FaRegPaperPlane} from "react-icons/fa";
+import {useFormStatus} from "react-dom";
 export default function Btn() {
-    const { pending } = useFormStatus();
+    const {pending} = useFormStatus();
     return (
         <button type="submit" disabled={pending} className={"group hover:scale-110 active:scale-105 transition rounded-full text-base font-semibold h-[3rem] w-[8rem] bg-black text-white flex justify-center gap-2 items-center disabled:opacity-75 disabled:scale-100 dark:bg-gray-700"}>
             {!pending ? (

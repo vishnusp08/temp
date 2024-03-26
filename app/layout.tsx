5f10da0d6659247ab9b599c18f3361950ffa3dff
Prajwalg19 <theimperical8@gmail.com>
2024-03-27 00:33:07 +0530

    tweaking responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
 import ActiveStateContext from "@/context/activeContext";
-const inter = Inter({ subsets: ["latin"] });
-import { Toaster } from "react-hot-toast";
+const inter = Inter({subsets: ["latin"]});
+import {Toaster} from "react-hot-toast";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ThemeContext from "@/context/themeContext";
@@ -23,17 +23,17 @@ export default function RootLayout({
             {/* this scroll smooth adds a smooth scrolling when the in page url is accessed using the id */}
             {/*the exlamation mark forces the property to be overriden in tailwind utility classes*/}
             <body className={`${inter.className} relative bg-slate-50 text-black dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-85 sm:pt-[10rem] pt-[7rem]`}>
-                <div className="-z-10 bg-green-100 dark:bg-green-900 blur-[30rem] full w-[55rem] h-[35rem] rounded-full absolute top-[2rem] left-[18rem] "></div>
-                <div className="-z-10 bg-purple-100 dark:bg-[#49063f] dark:blur-[100rem] w-[55rem] blur-[30rem] h-[35rem] rounded-full right-[12rem] absolute top-[6rem]"></div>
-                <Toaster />
-                <ActiveStateContext>
-                    <Header />
-                    <ThemeContext>
+                <div className="-z-10 bg-green-100 dark:bg-green-900 blur-[30rem] w-[30rem] -left-[20rem] sm:w-[55rem] h-[35rem] rounded-full absolute top-[2rem] sm:left-[-15rem] md:left-[-10rem] lg:left-[-5rem] xl:left-[20rem]"></div>
+                <div className="-z-10 bg-purple-100 dark:bg-[#49063f] dark:blur-[100rem] w-[30rem] sm:w-[55rem] blur-[30rem] h-[35rem] rounded-full right-[12rem] absolute top-[6rem]"></div>
+                <ThemeContext>
+                    <ActiveStateContext>
+                        <Toaster />
+                        <Header />
                         {children}
                         <ThemeSwitcher />
-                    </ThemeContext>
-                    <Footer />
-                </ActiveStateContext>
+                        <Footer />
+                    </ActiveStateContext>
+                </ThemeContext>
             </body>
         </html>
     );
